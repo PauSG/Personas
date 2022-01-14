50c4f6112072f771301d08232e231ce77eedcd7c
PauSG <pau.sanso@iesdoctorbalmis.com>
2022-01-14 11:53:20 +0100

    Agregar archivos de proyecto.

---
+++ Personas/MainWindowVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Personas
+{
+    class MainWindowVM
+    {
+        private UserControl opcion;
+
+        public UserControl Opcion
+        {
+            get { return opcion; }
+            set { opcion = value; }
+        }
+
+    }
+}
---
+++ Personas/NuevaPersona.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Personas
+{
+    /// <summary>
+    /// Lógica de interacción para NuevaPersona.xaml
+    /// </summary>
+    public partial class NuevaPersona : UserControl
+    {
+        public NuevaPersona()
+        {
+            InitializeComponent();
+            this.DataContext = new NuevaPersonaVM();
+        }
+    }
+}
